307baf5ec87acd3c67668103d0624830e7d7d6ae
ravikrjha20 <jhakrravi20@gmail.com>
2025-02-08 12:37:44 +0530

    Auth Controllers and send email

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -1,35 +1,36 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
-const createJWT =({payload})=>{
-    const token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME});
-    return token;
-}
+const createJWT = ({ payload }) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  return token;
+};
 
 // user is the tokenUser actually
-const attachCookiesToResponse = ({res,user,refreshToken})=>{
-    const accessTokenJWT = createJWT({payload:{user}});
-    const refreshTokenJWT=createJWT({payload:{user,refreshToken}});
-    
-    const oneDay = 1000*60*60*24;
-    const longerExp=1000*60*60*24*30;
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
 
-    res.cookie('accessToken',
-    accessTokenJWT,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==='production',signed:true,
-        expiresIn:new Date(Date.now()+oneDay),
-    })
-    
-    res.cookie('refreshToken',
-    refreshTokenJWT,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        signed:true,
-        expiresIn:new Date(Date.now()+longerExp),
-    });
-}
+  const oneDay = 1000 * 60 * 60 * 24;
+  const longerExp = 1000 * 60 * 60 * 24 * 30;
 
-const isTokenValid = ({token})=> jwt.verify(token,process.env.JWT_SECRET);
+  res.cookie("accessToken", accessTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+    expires: new Date(Date.now() + oneDay),
+  });
 
-module.exports ={createJWT,isTokenValid,attachCookiesToResponse};
+  res.cookie("refreshToken", refreshTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+    expires: new Date(Date.now() + longerExp),
+  });
+};
+
+const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
+
+module.exports = { createJWT, isTokenValid, attachCookiesToResponse };
