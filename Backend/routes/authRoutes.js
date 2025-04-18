8145a6aa16ccffb31c2c567212d88b5f9b754acb
NirwanGupta <guptanirwan@gmail.com>
2025-04-18 17:24:44 +0530

    small change

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -14,7 +14,7 @@ const { authenticateUser } = require("../middleware/authentication");
 const router = express.Router();
 
 router.post("/register", register);
-router.get("/check", checkAuth);
+router.get("/check", authenticateUser, checkAuth);
 router.post("/login", login);
 router.post("/logout", authenticateUser,logout);
 router.post("/verifyEmail", verifyEmail);
